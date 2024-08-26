5d1d488f0abb7bd470bcfb51879153c2f17391bf
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2024-08-26 11:31:27 +0200

    Created EOSPlayerController

---
+++ Source/EOS_OSS_Tutorial/EOSPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EOSPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOS_OSS_TUTORIAL_API AEOSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
