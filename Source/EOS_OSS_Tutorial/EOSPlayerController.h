21a4d52799f119be1eeb321a20fdfbae7e4e0d60
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2024-08-26 11:46:52 +0200

    Added PlayerController Login logic

--- Source/EOS_OSS_Tutorial/EOSPlayerController.h
+++ Source/EOS_OSS_Tutorial/EOSPlayerController.h
@@ -13,5 +13,24 @@ UCLASS()
 class EOS_OSS_TUTORIAL_API AEOSPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+private:
+
+	//Delegate to bind callback event for login. 
+	FDelegateHandle LoginDelegateHandle;
+
+public:
+
+	AEOSPlayerController();
+
+protected:
+
+	// Function called when play begins
+	virtual void BeginPlay();
+ 
+	//Function to sign into EOS Game Services
+	void Login();
+ 
+	//Callback function. This function is ran when signing into EOS Game Services completes. 
+	void HandleLoginCompleted(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
 };
