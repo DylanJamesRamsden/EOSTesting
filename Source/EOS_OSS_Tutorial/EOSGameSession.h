bcd684db47676e44f25732d35c9b8b7301200919
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2024-08-27 09:29:09 +0200

    Created EOSGameSession.h and .cpp

---
+++ Source/EOS_OSS_Tutorial/EOSGameSession.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameSession.h"
+#include "EOSGameSession.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOS_OSS_TUTORIAL_API AEOSGameSession : public AGameSession
+{
+	GENERATED_BODY()
+	
+};
